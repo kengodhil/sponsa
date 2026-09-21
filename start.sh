f08d66196dc17299663d08e34f9a8deed2b13ef6
Fadhili Mhanga <kengodhil@gmail.com>
2026-09-21 13:32:24 -0700

    Seed demo data in the Render start script

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 set -o errexit
 python backend/manage.py migrate --noinput
 python backend/manage.py collectstatic --noinput
+python backend/manage.py seed_demo
 exec gunicorn --chdir backend config.wsgi:application
