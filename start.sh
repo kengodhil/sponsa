934ece2bc8aff50979c56efb2dfeb9a14e2d829a
Fadhili Mhanga <kengodhil@gmail.com>
2026-09-21 13:13:49 -0700

    Collect static files in the Render start script

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 # Runtime entrypoint. Internal Postgres is reachable here.
 set -o errexit
 python backend/manage.py migrate --noinput
+python backend/manage.py collectstatic --noinput
 exec gunicorn --chdir backend config.wsgi:application
